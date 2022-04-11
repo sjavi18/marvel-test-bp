d7e33afd013877aff274fe8edb4c0c9738d6d299
Javier Simbana <wsimbana@pichincha.com>
2022-04-10 22:23:30 -0500

    JSI: Final

--- src/app/components/base/base.component.spec.ts
+++ src/app/components/base/base.component.spec.ts
@@ -9,32 +9,34 @@ import { HeaderComponent } from '../marvel/header/header.component';
 
 import { BaseComponent } from './base.component';
 import { ICharacter } from '../../models/character.model';
-import { of, Observable } from 'rxjs';
-import { CookieService } from 'ngx-cookie-service';
 
 window.scrollTo = jest.fn();
 describe('BaseComponent', () => {
   let component: BaseComponent;
   let fixture: ComponentFixture<BaseComponent>;
-  let marvelService: MarvelService
-  let marvelServiceMock: any
-  let fixtureTwo
+  let marvelService: MarvelService;
 
-  const responseAll: Observable<ICharacter[]> = of([{
+  const responseAll = [{
     _id: "623bb12b12979d2c2b04aad4",
     title: "Iron Man",
     body: "Inventor Tony Stark applies his genius for high-tech solutions to problems as Iron Man, the armored Avenger.",
     image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/002irm_ons_crd_03.jpg",
     category: "main", "idAuthor": "70", "createdAt": "2022-03-03T01:37:01.828Z", "updatedAt": "2022-03-03T01:37:01.828Z"
-  }])
-
+  }]
+  
+  const characterTest: ICharacter = {
+    title: "Javy",
+    body: "Super Javy",
+    image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/002irm_ons_crd_03.jpg",
+    category: "main", 
+    idAuthor: "70"
+  }
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BaseComponent, HeaderComponent,
         CharacterCardComponent,
         CharacterFormComponent,],
-      imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule],
-      providers: [MarvelService, CookieService]
+      imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule]
     })
       .compileComponents();
   });
@@ -43,16 +45,86 @@ describe('BaseComponent', () => {
     fixture = TestBed.createComponent(BaseComponent);
     component = fixture.componentInstance;
     marvelService = TestBed.inject(MarvelService);
-
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-
+  it('should retrieve all characters', () => {
     marvelService.getAllCharacters().subscribe(data => {
-      expect(data).toBe(0);
+      expect(data).toEqual(responseAll);
     })
+  });
 
+  it('should retrieve the characters depending on the search', () => {
+    marvelService.searchCharacter('iron').subscribe(data => {
+      expect(data).toEqual(responseAll[0]);
+    })
+  });
+
+  it('should create the requested character', () => {
+    marvelService.createCharacter(characterTest).subscribe(data => {
+      expect(data.message).toEqual('Nuevo personaje agregado!');
+    })
+  });
+
+  it('should edit the requested character', () => {
+    marvelService.editCharacter(characterTest, responseAll[0]._id).subscribe(data => {
+      expect(data.message).toEqual('Personaje actualizado');
+    })
+  });
+
+  it('should delete the requested character', () => {
+    marvelService.deleteCharacter(responseAll[0]._id).subscribe(data => {
+      expect(data.message).toEqual('Personaje eliminado');
+    })
+  });
+
+  it('should call handlerCancel', () => {
+    jest.spyOn(component, 'handlerCancel' as never);
+    component.handlerCancel('c')
+    expect(component.handlerCancel).toHaveBeenCalled()
+  });
+
+  it('should call handlerCancel', () => {
+    jest.spyOn(component, 'handlerCancel' as never);
+    component.handlerCancel('c')
+    expect(component.handlerCancel).toHaveBeenCalled()
+  });
+
+  it('should call handlerLogout', () => {
+    jest.spyOn(component, 'handlerLogout' as never);
+    component.handlerLogout()
+    expect(component.handlerLogout).toHaveBeenCalled()
+  });
+
+  it('should call handlerNewEvent', () => {
+    jest.spyOn(component, 'handlerNewEvent' as never);
+    component.handlerNewEvent()
+    expect(component.handlerNewEvent).toHaveBeenCalled()
+  });
+
+  it('should call processAction to save', () => {
+    jest.spyOn(component, 'processAction' as never);
+    component.processAction(characterTest)
+    expect(component.processAction).toHaveBeenCalled()
+  });
+
+
+  it('should call editCharacter', () => {
+    jest.spyOn(component, 'editCharacter' as never);
+    component.editCharacter(characterTest)
+    expect(component.editCharacter).toHaveBeenCalled()
+  });
+
+  it('should call deleteCharacter', () => {
+    jest.spyOn(component, 'deleteCharacter' as never);
+    component.deleteCharacter(characterTest)
+    expect(component.deleteCharacter).toHaveBeenCalled()
+  });
+
+  it('should call searchCharacter', () => {
+    jest.spyOn(component, 'searchCharacter' as never);
+    component.searchCharacter('spider')
+    expect(component.searchCharacter).toHaveBeenCalled()
   });
 
 });
--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -2,12 +2,36 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { LoginService } from '../../services/login/login.service';
 
 import { LoginComponent } from './login.component';
+import { IUser } from '../../models/user.model';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let loginService: LoginService;
+
+  const userTest: IUser = {
+    "email": "user4@gmail.com", 
+    "password": "password",
+    "authorid": "1"
+  }
+
+  const loginResp = {
+    "success": true,
+    "data": {
+      "id": 8,
+      "email": "user2@gmail.com",
+      "authorid": 1,
+      "jwt": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiZW1haWwiOiJ1c2VyMkBnbWFpbC5jb20iLCJhdXRob3JpZCI6MSwiaWF0IjoxNjQ5NjQ0ODcxLCJleHAiOjE2NDk2NDg0NzF9.IixvScbCFzSXhpMEcLhKBMt2JsdYS3_Hf5zUOmVx--0"
+    }
+  }
+
+  const signupResp = {
+    "success": true,
+    "message": "Usuario registrado exitosamente."
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,10 +44,58 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    loginService = TestBed.inject(LoginService);
+    
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create with signup', () => {
+    component.hasSignUp = true
+    
     expect(component).toBeTruthy();
+  });
+
+  it('should create without signup', () => {
+    component.hasSignUp = false
+    expect(component).toBeTruthy();
+  });
+
+  it('should register a user', () => {
+    loginService.signup(userTest).subscribe(data => {
+      expect(data).toEqual(signupResp);
+    })
+  });
+
+  it('should login a user', () => {
+    loginService.login(userTest).subscribe(data => {
+      expect(data).toEqual(loginResp);
+    })
+  });
+
+  it('should call handleSignUp', () => {
+    jest.spyOn(component, 'handleSignUp' as never);
+    component.handleSignUp()
+    expect(component.handleSignUp).toHaveBeenCalled()
+  });
+
+  it('should call send without signup', () => {
+    component.hasSignUp = false
+    jest.spyOn(component, 'send' as never);
+    component.send()
+    expect(component.send).toHaveBeenCalled()
+  });
+
+  it('should call getTitle with signup', () => {
+    component.hasSignUp = true
+    jest.spyOn(component, 'getTitle' as never);
+    component.getTitle()
+    expect(component.getTitle).toHaveBeenCalled()
+  });
+
+  it('should call getLabelLink with signup', () => {
+    component.hasSignUp = true
+    jest.spyOn(component, 'getLabelLink' as never);
+    component.getLabelLink()
+    expect(component.getLabelLink).toHaveBeenCalled()
   });
 });
